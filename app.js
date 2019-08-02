52ac795a4091d1c02d127304f13e9fc50309bdc2
kedbirhan <kedirbirhan@yahoo.com>
2019-08-02 15:08:17 -0400

    test

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 app.locals.moment = require('moment');
+//tedbjhdbjdbkdj
 
 // seedDB(); //seed the database
 
