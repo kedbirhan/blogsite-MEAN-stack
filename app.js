a1f5a1077797f245257c4f2ef0e878757fb90f01
kedbirhan <kedirbirhan@yahoo.com>
2019-08-02 01:46:12 -0400

    changed port

--- app.js
+++ app.js
@@ -58,12 +58,12 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(3000, function(){
-   console.log("The YelpCamp Server Has Started!");
-});
-// app.listen(process.env.PORT, process.env.IP, function(){
-//    console.log(process.env);
-//    console.log(process.env.IP);
+// app.listen(3000, function(){
 //    console.log("The YelpCamp Server Has Started!");
-
 // });
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log(process.env);
+   console.log(process.env.IP);
+   console.log("The YelpCamp Server Has Started!");
+
+});
